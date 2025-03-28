bcb2707c7f7e63efeb9cb8dc031483a77d2ff302
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-28 17:21:16 +0300

    Updates DBPassword to allow access mysql on localhost

--- config/env.go
+++ config/env.go
@@ -31,7 +31,7 @@ func initConfig() Config {
 		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
 		Port: 		getEnv("PORT", "8080"),
 		DBUser: 	getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
+		DBPassword: getEnv("DB_PASSWORD", "root"), // The password that I set to access mysql on localhost
 		DBAddress: 	fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")), // Dynamically added
 		DBName: getEnv("DB_NAME", "ecommerce"),
 		
