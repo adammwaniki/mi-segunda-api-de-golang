ae1329a621fea13bdbb80be3156b84931898d7d2
Adam Mwaniki <adamndegwa@protonmail.com>
2025-04-07 14:36:38 +0300

    Successfully registers product service on api pending POST products

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/adammwaniki/mi-segunda-api-de-golang/service/product"
 	"github.com/adammwaniki/mi-segunda-api-de-golang/service/user"
 	"github.com/gorilla/mux"
 )
@@ -27,9 +28,15 @@ func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
+	// Register User service on the api
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore) // dependency injection
 	userHandler.RegisterRoutes(subrouter)
+
+	// Register product service on the api
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("Server running on", s.addr)
 	return http.ListenAndServe(s.addr, router)
